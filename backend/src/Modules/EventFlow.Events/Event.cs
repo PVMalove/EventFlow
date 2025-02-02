25923d25586896b9e01a0e59fd91436090ff4012
Pavel Malove <107810341+PVMalove@users.noreply.github.com>
2025-02-02 20:59:34 +0300

    up

---
+++ backend/src/Modules/EventFlow.Events/Event.cs
@@ -0,0 +1,16 @@
+﻿namespace EventFlow.Events;
+
+public sealed class Event
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public string Location { get; set; }
+
+    public DateTime StartsAtUtc { get; set; }
+
+    public DateTime? EndsAtUtc { get; set; }
+}
